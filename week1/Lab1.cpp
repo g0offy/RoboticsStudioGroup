f9c25c22ad1a768ebcc732f7fcf5836c9b6ecbb6
Bhupesh Guddi <5Bhupeshguddi@gmail.com>
2024-08-20 23:43:49 +1000

    Week2 lab work

---
+++ week1/Lab1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+// Include msg type
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/Image.hpp"
+
+int main(void)
+{
+    printf("TEST1\n");
+    return 0;
+}
